52c97f8840ce37076d620f3d700a8152be221dc0
Nelson Lai <nelsonlai9503@gmail.com>
2022-12-04 07:21:24 +0000

    run engine with initialized engine within package

--- ginger/engine.go
+++ ginger/engine.go
@@ -16,10 +16,10 @@ func init() {
 // If GIN_MODE == release, the host will be 0.0.0.0
 // Otherwise, the host will be 127.0.0.1
 // The port is always set to 5000
-func Run(engine *gin.Engine) {
+func Run() {
 	if os.Getenv("GIN_MODE") == "release" {
-		engine.Run("0.0.0.0:5000")
+		Engine.Run("0.0.0.0:5000")
 	} else {
-		engine.Run("127.0.0.1:5000")
+		Engine.Run("127.0.0.1:5000")
 	}
 }
