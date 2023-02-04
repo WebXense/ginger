16dab756c6c4a480fe0fb88725017ab9bc9b631f
Nelson Lai <nelsonlai9503@gmail.com>
2023-02-04 17:00:24 +0800

    refactor cors

--- cors/setup.go
+++ cors/setup.go
@@ -5,12 +5,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Config cors.Config
+
 // Setup setup cors
-func Setup(engine *gin.Engine, config cors.Config) {
-	engine.Use(cors.New(config))
+func Setup(engine *gin.Engine, config Config) {
+	engine.Use(cors.New(cors.Config(config)))
 }
 
 // SetupDefault setup cors using default config
 func SetupDefault(engine *gin.Engine) {
-	engine.Use(cors.New(cors.DefaultConfig()))
+	engine.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
 }
